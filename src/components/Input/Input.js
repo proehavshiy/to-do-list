e62b11ed7dff5c55e4dd2b5fbf2a7f8dc6f804be
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-04-27 00:52:48 +0300

    feat: clear input at the end via ref

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { createRef } from 'react'
 import classNames from 'classnames/bind'
 import styles from './Input.module.css'
 
@@ -10,17 +10,21 @@ const cn = classNames.bind(styles);
 
 function Input() {
   const dispatch = useDispatch()
+  const inputRef = createRef(null)
 
   return (
     <form name="form" noValidate autoComplete="off"
       onSubmit={(e) => {
         e.preventDefault()
-        const newToDoPayload = e.target.children[0].value
+        // const newToDoPayload = e.target.children[0].value
+        const newToDoPayload = inputRef.current.value
         // update store
         dispatch(addNewToDo(newToDoPayload))
+        // clear input at the end
+        inputRef.current.value = ''
       }}
     >
-      <input id="newTodo" className="new-todo" placeholder="What needs to be done?" />
+      <input id="newTodo" className="new-todo" placeholder="What needs to be done?" ref={inputRef} />
     </form>
   )
 }
